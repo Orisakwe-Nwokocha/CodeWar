fb3bc7fed44763b6415f5da4da6578c79b626890
Orisha <orisakwenwokocha1@gmail.com>
2023-12-07 17:15:17 +0100

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,23 @@
+public class Array {
+
+    public static void main(String[] args) {
+
+	int[] array = {3, 4, 5, 6, 7};
+	
+
+	for (int i = 0; i < array.length; i++){
+		array[i] *= array[i];
+		System.out.print(array[i] + ", ");
+
+	}
+
+
+	
+
+   }
+
+}
+
+
+
+
